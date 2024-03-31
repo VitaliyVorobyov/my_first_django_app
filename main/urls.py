3fb7b0c4463fb51dc9ed827add4ce0ac6b7d8c73
VitaliyVorobyov <123983999+VitaliyVorobyov@users.noreply.github.com>
2024-03-31 22:50:19 +0300

    bag fix

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('message', views.message, name='message'),
+    path('pages', views.pages, name='pages'),
+    path('time_now', views.time_now, name='time_now'),
+    path('directory', views.directory, name='directory'),
     path('<int:pk>', views.MessageDetailView.as_view(), name='message_view'),
     path('<int:pk>/update', views.MessageUpdateView.as_view(), name='message_update'),
     path('<int:pk>/delete', views.MessageDeleteView.as_view(), name='message_delete'),
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import DetailView, UpdateView, DeleteView
+import os
+from datetime import datetime
 from .forms import MainForm
 from .models import MyModel
 
@@ -46,3 +48,34 @@ def message(request):
     }
 
     return render(request, 'main/message.html', data)
+
+
+def pages(request):
+    data = {
+        'values': [
+            f"{reverse('index')} - Главная страница",
+            f"{reverse('message')} - Сообщения",
+            f"{reverse('pages')} - Страницы",
+            f"{reverse('time_now')} - Текущее время",
+            f"{reverse('directory')} - Каталог"
+        ]
+    }
+    return render(request, 'main/pages.html', data)
+
+
+def time_now(request):
+    day = datetime.now().day
+    month = datetime.now().month
+    year = datetime.now().year
+    hour = datetime.now().hour
+    minute = datetime.now().minute
+    data = {
+        'values': [f'Год: {year}', f'Месяц: {month}', f'День: {day}', f'Час: {hour}', f'Минута: {minute}']
+    }
+    return render(request, 'main/time_now.html', data)
+
+
+def directory(request):
+    workdir = os.getcwd()
+    data = {'values': os.listdir(workdir)}
+    return render(request, 'main/directory.html', data)
